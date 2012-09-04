25c597239178f3d4fff34edd458407bda8ec6af4
Mathieu Lecarme <mlecarme@bearstech.com>
2012-09-04 13:54:09 +0200

    Pygments POC.

--- beautifier/html.py
+++ beautifier/html.py
@@ -1,8 +1,12 @@
 from lxml import etree
 from cStringIO import StringIO
+from pygments import highlight
+from pygments.lexers import HtmlLexer
+from pygments.formatters import Terminal256Formatter
 
 
 def parse(txt):
     parser = etree.HTMLParser()
     tree = etree.parse(StringIO(txt), parser)
-    return etree.tostring(tree.getroot(), pretty_print=True, method="html")
+    indent = etree.tostring(tree.getroot(), pretty_print=True, method="html")  # FIXME
+    return highlight(indent, HtmlLexer(), Terminal256Formatter())
